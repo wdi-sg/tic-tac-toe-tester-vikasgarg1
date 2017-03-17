d0f03bf7fc6153c12aa036fb77ac1e5fbb3fe7e7
Vikas Garg <vikasgarg@hotmail.com>
2017-03-17 17:20:45 +0800

    latest code

---
+++ testing-the-tester.js
@@ -0,0 +1,15 @@
+// this is where we will write code this weekend
+
+function restart() {
+
+}
+
+function isGameOver() {
+  return false
+}
+function whoWon() {
+  return 0
+}
+function playTurn() {
+
+}
